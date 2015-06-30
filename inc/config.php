df93b3f6d08baa697296311aa3c89d1634d6fab2
Webforce3 <denis.nerborac@wf3.fr>
2015-06-30 14:46:00 +0200

    FIX inc/config - update facebook root path

--- inc/config.php
+++ inc/config.php
@@ -2,7 +2,7 @@
 session_name('movies_session');
 session_start();
 
-define('FACEBOOK_SDK_ROOT_PATH', 'inc/facebook');
+define('FACEBOOK_SDK_ROOT_PATH', 'facebook');
 
 define('FACEBOOK_SDK_V4_SRC_DIR', FACEBOOK_SDK_ROOT_PATH.'/src/Facebook/');
 require __DIR__ . '/'.FACEBOOK_SDK_ROOT_PATH.'/autoload.php';
