633b314ec29bffa51701b29e267031b16496751c
Webforce3 <denis.nerborac@wf3.fr>
2015-06-30 14:53:56 +0200

    Update inc/config - Remove FB API credentials

--- inc/config.php
+++ inc/config.php
@@ -7,8 +7,8 @@ define('FACEBOOK_SDK_ROOT_PATH', 'facebook');
 define('FACEBOOK_SDK_V4_SRC_DIR', FACEBOOK_SDK_ROOT_PATH.'/src/Facebook/');
 require __DIR__ . '/'.FACEBOOK_SDK_ROOT_PATH.'/autoload.php';
 
-define('FB_APP_ID', '911544085571972');
-define('FB_APP_SECRET', '7783da3269c3fd0925e796904703d867');
+define('FB_APP_ID', '');
+define('FB_APP_SECRET', '');
 
 $root_path = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
 
